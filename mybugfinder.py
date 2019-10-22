56b2fcbfa9fbefd1714eeb455643e546057763f2
ghao7 <guhao6869@gmail.com>
2019-10-22 17:44:46 -0400

    first commit

---
+++ mybugfinder.py
@@ -0,0 +1,38 @@
+import javalang
+
+
+def is_string(tokens, i, string_set):
+    if type(tokens[i]) == javalang.tokenizer.String:
+        return True
+    if type(tokens[i]) == javalang.tokenizer.Identifier and tokens[i].value in string_set:
+        return True
+    return False
+
+
+def is_operator(tokens, i):
+    return type(tokens[i]) == javalang.tokenizer.Operator
+
+
+def parse_file(fname):
+    result = list()
+    string_set = set()
+    with open(fname, 'r') as file:
+        data = file.read()
+        tokens = list(javalang.tokenizer.tokenize(data))
+        for i in range(len(tokens)):
+            # print(tokens[i])
+            if type(tokens[i]) == javalang.tokenizer.Identifier and tokens[i].value == "String":
+                string_set.add(tokens[i+1].value)
+            if i < len(tokens) - 2 and is_string(tokens, i, string_set) and is_operator(tokens, i+1)\
+                    and is_string(tokens, i + 2, string_set):
+                if tokens[i].position[0] != tokens[i+1].position[0] or \
+                        tokens[i+1].position[0] != tokens[i + 2].position[0]:
+                    continue
+                if javalang.tokenizer.Operator.is_infix(tokens[i+1]):
+                    result.append(tokens[i+1].position)
+
+    print(result)
+
+
+fname = 'test.java'
+parse_file(fname)
---
+++ test.java
@@ -0,0 +1,14 @@
+public class Test {
+    public static void main(String[] args){
+    	if ("walmart" == "offer"){
+    	    return;
+    	}
+    	if ("walmart" != "offer"){
+    	    return;
+    	}
+    	String s = "A";
+    	if (s == "offer"){
+    	    return;
+    	}
+    }
+}
